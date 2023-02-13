9810b90a08409b2d39241a1627f6c116c2ad901e
Sarmad Hussain <103625470+zsarmad@users.noreply.github.com>
2023-02-13 22:46:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const WebSocket = require('ws');
+
+const PORT = 5000;
+
+const wsServer = new WebSocket.Server({
+    port: PORT
+});
+
+wsServer.on('connection', function (socket) {
+    // Some feedback on the console
+    console.log("A client just connected");
+
+    // Attach some behavior to the incoming socket
+    socket.on('message', function (msg) {
+        console.log("Received message from client: "  + msg);
+        // socket.send("Take this back: " + msg);
+
+        // Broadcast that message to all connected clients
+        wsServer.clients.forEach(function (client) {
+            client.send("Someone said: " + msg);
+        });
+
+    });
+
+    socket.on('close', function () {
+        console.log('Client disconnected');
+    })
+
+});
+
+console.log( (new Date()) + " Server is listening on port " + PORT);
